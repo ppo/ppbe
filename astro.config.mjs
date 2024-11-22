8504a54df01e8df0afcfdf23b768c8b735521488
Pascal Polleunus <pascal@polleunus.be>
2024-11-22 20:27:34 +0400

    Configure Vercel deployment.
    - Add `vercel` adapter.
      - `pnpm astro add vercel`
      - Configure Astro.
    - Add configuration for Vercel:
      - Define Node version in `package.json`.
      - Create `vercel.json` to define install command.
    - Git ignore `.vercel` folder.
    - Install Vercel CLI.
      - `brew install vercel-cli`
    - Deploy to Vercel, accepting all defaults.
      - `vercel deploy --yes`.
      - Test: `https://ppbe.vercel.app/`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
+
 import { APP_SLUG, SITE_URL } from './src/settings.mjs'; // Note: Import aliases don't work here.
 
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
+
   site: SITE_URL,
   server: { // Used by `astro dev` & `astro preview`
     host: `${APP_SLUG}.test`,

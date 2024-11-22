02165a479ae13a8efdb5f04e33b08341a2f08d98
Pascal Polleunus <pascal@polleunus.be>
2024-11-23 03:47:09 +0400

    Integrate Tailwind CSS.
    - Integrate `Tailwind`.
      - `pnpm astro add tailwind`
      - Install minimal `tailwind.config.mjs`.
      - Configure Astro.
    - Install Tailwind Typography plugin.
      - `pnpm install -D @tailwindcss/typography`
      - Configure Tailwind.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 import partytown from '@astrojs/partytown';
 import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 
 import { APP_SLUG, SITE_URL } from './src/settings.mjs'; // Note: Import aliases don't work here.
@@ -16,6 +17,7 @@ export default defineConfig({
   integrations: [
     partytown(),
     sitemap(),
+    tailwind(),
   ],
 
   site: SITE_URL,

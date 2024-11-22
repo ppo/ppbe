a6ac956372f757ef84989eafcf85d21bdc35f524
Pascal Polleunus <pascal@polleunus.be>
2024-11-22 20:03:16 +0400

    Configure project.
    - Empty `README.md`.
    - Create global settings.
    - Configure Astro.
    - Configure import aliases.
    - Create layout.
    - Review `.gitignore`.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import { APP_SLUG, SITE_URL } from './src/settings.mjs'; // Note: Import aliases don't work here.
+
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE_URL,
+  server: { // Used by `astro dev` & `astro preview`
+    host: `${APP_SLUG}.test`,
+  },
+});
---
+++ src/settings.mjs
@@ -0,0 +1,24 @@
+export const APP_SLUG = 'ppbe';
+export const SITE_URL = 'https://pascal.polleunus.be';
+export const SITE_NAME = 'Pascal Polleunus';
+
+export const MY_NAME = 'Pascal Polleunus';
+export const MY_EMAIL = 'pascal@polleunus.be';
+
+
+// HEAD ============================================================================================
+
+// Settings for the HTML <head>.
+export const HTML_HEAD = {
+  title: SITE_NAME,
+  titleSeparator: ' :: ',
+  description: 'Pascal Polleunus’ personal website.',
+};
+
+
+// ROUTES & URLS ===================================================================================
+
+// Named routes (as name: path).
+export const ROUTES = {
+  home: '/',
+};

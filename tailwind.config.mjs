8db4d2ffd7758cd3e1231e62a2b886be3e10dc0e
Pascal Polleunus <pascal@polleunus.be>
2024-11-23 05:22:38 +0400

    Base design.
    Configure Tailwind:
    - Base: fonts, headings.
    - Layout: header, main, footer.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,39 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+import { FONTS } from './src/settings.mjs';
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      display: [FONTS.display.name],
+      sans: [FONTS.body.name, ...defaultTheme.fontFamily.sans],
+      // serif: ['???', ...defaultTheme.fontFamily.serif],
+    },
+    extend: {
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            'h1, h2, h3, h4, h5, h6': {
+              fontFamily: theme('fontFamily.display'),
+              fontWeight: 700,
+            },
+          },
+        },
+      }),
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
+    function({ addBase, theme }) {
+      addBase({
+        '#main-header h1': {
+          fontFamily: theme('fontFamily.sans'),
+          fontWeight: 300,
+        },
+      });
+    },
   ],
 }

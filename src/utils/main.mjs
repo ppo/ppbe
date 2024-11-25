81a830258dd7e3464ec14f5c65efca29db02394e
Pascal Polleunus <pascal@polleunus.be>
2024-11-25 19:22:22 +0400

    Review head title and description handling.

--- src/utils/main.mjs
+++ src/utils/main.mjs
@@ -1,3 +1,6 @@
+import { HTML_HEAD } from '@/settings.mjs';
+
+
 /**
  * Returns the first argument that is not `undefined`.
  */
@@ -5,4 +8,13 @@ export function getFirstDefined() {
   for (const arg of arguments) {
     if (arg !== undefined) return arg;
   }
+}
+
+
+/**
+ * @param {...(string|undefined)} titles - Title(s) to be merged.
+ * @returns {string} - The merged title, or the default title.
+ */
+export function joinHeadTitle(...titles) {
+  return titles.filter(a => a).join(HTML_HEAD.titleSeparator);
 }

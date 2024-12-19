72f2431b62c6da4bd51ea9c23605b5f863cec24f
Pascal Polleunus <pascal@polleunus.be>
2024-12-19 13:49:17 +0800

    Review blog article props order. Fix: Use proper timezone & change Zod config.

---
+++ src/content/blog/_template.md
@@ -0,0 +1,25 @@
+---
+slug: 
+publishedAt: 2000-12-31T23:59:59+0200  # Shell: date "+%Y-%m-%dT%H:%M:%S%z" | pbcopy
+createdAt: 
+title: 
+subtitle: 
+abstract: 
+featured: true
+category: 
+tags: ["<category>"] 
+hero:
+  image: /blog/
+  video: 
+  alt: 
+  title: 
+  subtitle: 
+  url: 
+  caption: 
+  captionUrl: 
+  captionIsHtml: true
+  class: 
+---
+
+
+## 
--- src/content/config.mjs
+++ src/content/config.mjs
@@ -1,7 +1,7 @@
 import { defineCollection, z } from 'astro:content';
 
 const hero = z.object({
-  image: z.string().nullish(),
+  image: z.string().nullish(), // Should provide `image` or `video`
   video: z.string().nullish(),
   alt: z.string().nullish(),
   title: z.string().nullish(),
@@ -9,27 +9,26 @@ const hero = z.object({
   url: z.string().nullish(),
   caption: z.string().nullish(),
   captionUrl: z.string().nullish(),
-  captionIsHtml: z.boolean().nullish(),
+  captionIsHtml: z.boolean().nullish(), // Default: false
   class: z.string().nullish(),
 });
 
+// TODO: Dates… z.date() OR z.string().datetime() OR z.coerce.date() // Transform string to Date object
 const blog = defineCollection({
   type: 'content',
   schema: z.object({
+    slug: z.string().nullish(),
+    publishedAt: z.coerce.date().nullable(), // The reference date for sorting
+    createdAt: z.coerce.date().nullish(), // When the content was originally created, defaults to publishedAt.
     title: z.string(),
     subtitle: z.string().nullish(),
     abstract: z.string().nullish(),
     featured: z.boolean().nullish(),
     category: z.string().nullish(),
     tags: z.array(z.string()).nullish(),
-    hero: hero.nullish(),
     // collection: z.string().nullish(),
     // related: z.array(reference('blog')).nullable(),  // References to other blog articles, using their `slug`.
-    // author: z.string(),
-    publishedAt: z.date().nullable(), // The reference date for sorting
-    createdAt: z.date().nullish(), // When the content was originally created, defaults to publishedAt.
-    // OR? z.string().datetime()
-    //OR? z.coerce.date() // Transform string to Date object
+    hero: hero.nullish(),
   }),
 });
 
